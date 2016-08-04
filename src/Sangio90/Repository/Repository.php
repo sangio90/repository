e2ea0ec5c1851a9178db17930309af9d22c55991
sangio1990 <sangiovanni.guido@gmail.com>
2016-08-04 20:09:05 +0200

    attributes added: tableName default values for attributes added. read method now split in 2 parts, one that executes the read and one that handles the parameters. basic insert added

--- src/Sangio90/Repository/Repository.php
+++ src/Sangio90/Repository/Repository.php
@@ -1,19 +1,47 @@
 <?php
 namespace Sangio90\Repository;
 
+use Doctrine\DBAL\Query\QueryBuilder;
 
 class Repository
 {
-    protected $stmt;
+    protected $stmt = '';
+
     /** @var  \Doctrine\DBAL\Connection */
     protected $connection;
-    protected $fields = [];
-    protected $tableName;
 
-    public function read()
+    protected $fields = [];
+
+    protected $tableName = '';
+
+    public function read($parameters = [])
     {
         $queryBuilder = $this->connection->createQueryBuilder();
-        return $queryBuilder->select("*")->from("(" . $this->stmt . ") as result")->execute()->fetchAll();
+        $query = $queryBuilder->select("*")->from("(" . $this->stmt . ") as result");
+        $query = $this->parseParameters($query, $parameters);
+        return $query->execute()->fetchAll();
+    }
+
+    /**
+     * @param QueryBuilder $query
+     * @param array $parameters
+     * @return QueryBuilder
+     */
+    protected function parseParameters(QueryBuilder $query, $parameters = [])
+    {
+        if (array_key_exists('order', $parameters)) {
+            $orderRule = $parameters['order'];
+            $orderField = $orderRule['field'];
+            $orderDirection = $orderRule['direction'] ? $orderRule['direction'] : 'ASC';
+            $query = $query->orderBy($orderField, $orderDirection);
+        }
+        if (array_key_exists('start', $parameters)) {
+            $query = $query->setFirstResult($parameters['start']);
+        }
+        if (array_key_exists('limit', $parameters)) {
+            $query = $query->setMaxResults($parameters['limit']);
+        }
+        return $query;
     }
 
     public function insert($row)
@@ -23,6 +51,9 @@ class Repository
         $values = [];
         foreach ($this->fields as $field) {
 
+            /**
+             * If the field is the identifier (supposed to be auto_increment) it should not be written
+             */
             if ($field['id']) continue;
 
             $fieldName = $field['name'];
