66c0ce79fc418a595e204cc3df171eaa9b5b1114
sangio1990 <sangiovanni.guido@gmail.com>
2016-08-04 12:42:57 +0200

    insert

--- src/Sangio90/Repository/Repository.php
+++ src/Sangio90/Repository/Repository.php
@@ -7,11 +7,28 @@ class Repository
     protected $stmt;
     /** @var  \Doctrine\DBAL\Connection */
     protected $connection;
+    protected $fields = [];
+    protected $tableName;
 
     public function read()
     {
         $queryBuilder = $this->connection->createQueryBuilder();
         return $queryBuilder->select("*")->from("(" . $this->stmt . ") as result")->execute()->fetchAll();
+    }
+
+    public function insert($row)
+    {
+        $queryBuilder = $this->connection->createQueryBuilder();
+        $data = [];
+        $values = [];
+        foreach ($this->fields as $field) {
+            $fieldName = $field['name'];
+            $data[] = [
+                $fieldName['name'] => '?'
+            ];
+            $values[] = $row[$fieldName];
+        }
+        $queryBuilder->insert($this->tableName)->values($data)->setParameters($values)->execute();
     }
 
 }
